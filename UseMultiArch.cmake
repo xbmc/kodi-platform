d34260244f3334c56c27ec4dc33098953cb808bc
Wolfgang Haupt <haupt.wolfgang@gmail.com>
2015-09-09 20:51:12 +0200

    fixed: libdir in UseMultiArch.cmake default to lib

--- UseMultiArch.cmake
+++ UseMultiArch.cmake
@@ -20,7 +20,10 @@ if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux" AND
   if (EXISTS "/etc/debian_version")
 	set (_libdir_def "lib/${CMAKE_LIBRARY_ARCHITECTURE}")
 	set (_libdir_noarch "lib")
-  else (EXISTS "/etc/debian_version")
+  elseif (EXISTS "/etc/fedora-release" OR
+          EXISTS "/etc/redhat-release" OR
+          EXISTS "/etc/slackware-version" OR
+          EXISTS "/etc/gentoo-release")
 	# 64-bit system?
 	if (CMAKE_SIZEOF_VOID_P EQUAL 8)
 	  set (_libdir_noarch "lib64")
@@ -28,7 +31,10 @@ if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux" AND
 	  set (_libdir_noarch "lib")
 	endif (CMAKE_SIZEOF_VOID_P EQUAL 8)
 	set (_libdir_def "${_libdir_noarch}")
-  endif (EXISTS "/etc/debian_version")
+  else ()
+    set (_libdir_def "lib")
+    set (_libdir_noarch "lib")
+  endif ()
 else ()
   set (_libdir_def "lib")
   set (_libdir_noarch "lib")
